406d8f63cd020632166953bd04e92edf569c9182
mbob72 <mbob72@gmail.com>
2026-06-26 22:19:41 +0300

    add yandex metrika

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { Header } from '../src/components/Header';
 import { SITE_TITLE } from '../src/data/brand';
 import { getHomeSeo, toMetadata } from '../src/data/seo';
@@ -20,6 +21,23 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="ru">
       <body>
+        <Script id="yandex-metrika" strategy="beforeInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=110180836', 'ym');
+
+            ym(110180836, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/110180836" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
         <div className="flex h-dvh flex-col overflow-hidden bg-white text-ink">
           <Header />
           <main className="flex min-h-0 flex-1 flex-col overflow-hidden">{children}</main>
